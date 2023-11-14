560692e61370ed09fdc1337d8d71ca3b627a2027
Dyuti Barma <dyuti@currents.ai>
2023-11-14 19:10:27 +0530

    removed markdown-page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://currents-ai.github.io',
+  url: 'https://freetext.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs',
@@ -51,7 +51,7 @@ const config: Config = {
         sitemap: {
           //changefreq: 'weekly',
           priority: 0.5,
-          ignorePatterns: ['/tags/**', '/docs/blog/**'],
+          ignorePatterns: ['/tags/**', '/docs/blog/**', '/docs/markdown-page'],
           filename: 'sitemap.xml',
         },
         gtag: {
