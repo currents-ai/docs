d23851a73fcc921cc33e5fd33f5751817006cae5
Jantu Deb <108511272+currents-jantu@users.noreply.github.com>
2025-11-10 12:07:50 +0530

    fix:search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -171,7 +171,7 @@ const config: Config = {
     },
     algolia: { 
       appId: '6XF31BUUL9', // The application ID provided by Algolia
-      apiKey: 'da3f1b3152a8b6b73fe4dc997453d0f8', // Public API key: it is safe to commit it
+      apiKey: 'a7068e95edad7bc387f2549b0f2b3697', // Public API key: it is safe to commit it
       indexName: 'freetext',
       contextualSearch: true,  // Optional: see doc section below
 
